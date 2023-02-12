a977ac2dac067a1ab6cdb8e6c79e4848adc00923
Kyoung Tae <113958094+Park-KyoungTae@users.noreply.github.com>
2023-02-12 17:56:26 +0900

    [Bronze V] Title: A/B, Time: 124 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -1,15 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main{
-    public static void main(String args[]){
-        Scanner scan = new Scanner(System.in);
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        double A=scan.nextDouble();
-        double B=scan.nextDouble();
-
-        System.out.println(A/B);
+        String str= br.readLine();
         
-        scan.close();
+        StringTokenizer st = new StringTokenizer(str," ");
+        double a = Double.parseDouble(st.nextToken());
+        double b = Double.parseDouble(st.nextToken());
+        
+        System.out.println(a/b);
     }
-    
 }
